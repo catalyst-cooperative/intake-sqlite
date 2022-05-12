bbd693b60126718ee2f78d0f6ab49fa677bbcfb9
Zane Selvans <zane.selvans@catalyst.coop>
2022-05-11 22:12:59 -0500

    Add some nominal unit and integration tests.

--- tests/integration/intake_sqlite_test.py
+++ tests/integration/intake_sqlite_test.py
@@ -1,16 +1,24 @@
 """A dummy integration test so pytest has something to do."""
 import logging
 
-# from intake_sqlite import SQLiteCatalog, SQLiteSource
+from intake_sqlite import SQLiteCatalog, SQLiteSource
 
 logger = logging.getLogger(__name__)
 
 
 def test_sqlite_source() -> None:
     """Need to test basic ability to create and access an SQLiteSource."""
-    pass
+    gpp_src = SQLiteSource(
+        urlpath="https://global-power-plants.datasettes.com/global-power-plants.db",
+        sql_expr="SELECT * FROM 'global-power-plants'",
+    )
+    df = gpp_src.read()
+    assert df.shape == (34936, 36)  # nosec: B101
 
 
 def test_sqlite_catalog() -> None:
-    """Need to test basic ability to create and access an SQLiteCatalog."""
-    pass
+    """Test basic ability to create and access a SQLiteCatalog."""
+    gpp_cat = SQLiteCatalog(
+        urlpath="https://global-power-plants.datasettes.com/global-power-plants.db",
+    )
+    assert list(gpp_cat)[0] == "global-power-plants"  # nosec: B101
--- tests/unit/intake_sqlite_test.py
+++ tests/unit/intake_sqlite_test.py
@@ -1,26 +1,17 @@
 """A dummy unit test so pytest has something to do."""
 import logging
-
-import pytest
+from pathlib import Path
 
 from intake_sqlite import urlpath_to_sqliteurl
 
 logger = logging.getLogger(__name__)
 
+TEST_DIR = Path(__file__).parent.parent.resolve()
 
-@pytest.mark.parametrize(
-    "urlpath,expected_sqliteurl",
-    [
-        # ("gs://intake.catalyst.coop/test/test.db", "sqlite://"),
-        ("/home/zane/code/catalyst/intake-sqlite/tests/data/test.db", "sqlite://"),
-    ],
-)
-def test_something(urlpath: str, expected_sqliteurl: str) -> None:
-    """Test the dummy function from our dummy module to generate coverage.
 
-    This function also demonstrates how to parametrize a test.
-
-    """
-    actual_sqliteurl = urlpath_to_sqliteurl(urlpath)
-    logger.info(f"{urlpath=} {actual_sqliteurl=}")
-    # assert actual_sqliteurl == expected_sqliteurl  # nosec: B101
+def test_urlpath_to_sqliteurl() -> None:
+    """Test our transformation of paths/URLs into SQL Alchemy URLs."""
+    expected_local_url = "sqlite:///" + str(TEST_DIR / "data/test.db")
+    test_db_path = TEST_DIR / "data/test.db"
+    actual_local_url = urlpath_to_sqliteurl(str(test_db_path))
+    assert actual_local_url == expected_local_url  # nosec: B101
